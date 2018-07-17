0794b4873fadc1dee7b2f3f8a9503477dac05d0c
Unknown <az.kinosaki@gmail.com>
2018-07-17 21:46:11 +0900

    最初のcommit
    最初に行ったcommitです。

---
+++ Assets/Scripts/uiButtons.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class uiButtons : MonoBehaviour {
+
+    Animator canvas;
+    GameObject audioSource;
+
+    // Use this for initialization
+    void Start () {
+        canvas = GameObject.Find("Canvas").GetComponent<Animator>();
+        audioSource = GameObject.Find("Audio Source");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //マウスカーソルがボタンの上に乗ったとき、選択状態にする
+    public void MouseSelector()
+    {
+        EventSystem.current.SetSelectedGameObject(gameObject);
+    }
+
+    //ボタンが選択状態になったとき、アニメーションを再生する
+    public void ButtonHighlited()
+    {
+        audioSource.GetComponent<AudioSource>().Stop();
+        audioSource.GetComponent<AudioSource>().PlayOneShot(audioSource.GetComponent<AudioSource>().clip);
+
+        switch (EventSystem.current.currentSelectedGameObject.name)
+        {
+            case "quickButton":
+                canvas.ResetTrigger("artistHighlight");
+                canvas.ResetTrigger("settigsHighlight");
+                canvas.ResetTrigger("exitHighlight");
+
+                canvas.SetTrigger("quickHighlight");
+                break;
+
+            case "artistButton":
+
+                canvas.ResetTrigger("quickHighlight");
+                canvas.ResetTrigger("settigsHighlight");
+                canvas.ResetTrigger("exitHighlight");
+
+                canvas.SetTrigger("artistHighlight");
+
+                break;
+
+            case "settingsButton":
+
+                canvas.ResetTrigger("quickHighlight");
+                canvas.ResetTrigger("artistHighlight");
+                canvas.ResetTrigger("exitHighlight");
+
+                canvas.SetTrigger("settigsHighlight");
+
+                break;
+
+            case "exitButton":
+
+                canvas.ResetTrigger("quickHighlight");
+                canvas.ResetTrigger("artistHighlight");
+                canvas.ResetTrigger("settigsHighlight");
+
+                canvas.SetTrigger("exitHighlight");
+
+                break;
+
+        }
+    }
+
+    //ボタンがEnterキーで選択されたとき、Circleの座標をずらさずアニメーションを再生する
+    public void ButtonSubmit()
+    {
+        gameObject.transform.Find("Circle").gameObject.transform.localPosition = new Vector2(0,0);
+        gameObject.GetComponent<Animator>().SetTrigger("Submit");
+        ClickEvent();
+    }
+
+    //ボタンがマウスでクリックされたとき、Circleの座標をずらしてアニメーションを再生する
+    public void OnClick()
+    {
+        gameObject.transform.Find("Circle").gameObject.transform.position = Input.mousePosition;
+        gameObject.GetComponent<Animator>().SetTrigger("Submit");
+        ClickEvent();
+    }
+
+    //クリックイベント
+    public void ClickEvent() {
+
+        switch (gameObject.name) {
+
+            case "exitButton":
+                EventSystem.current.SetSelectedGameObject(null);
+                canvas.SetTrigger("exitDialogShow");
+
+                break;
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/uiKeyInputs.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class uiKeyInputs : MonoBehaviour {
+
+    bool flag = true,enableKeyInput = false;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(StartKeyInput());
+	}
+
+    IEnumerator StartKeyInput() {
+        yield return new WaitForSeconds(2.5f);
+        enableKeyInput = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //メニューの上下選択（デフォルト設定だけ）
+        if (EventSystem.current.currentSelectedGameObject == null && (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow)) && enableKeyInput && !GameObject.Find("exitDialog").GetComponent<Image>().raycastTarget)
+        {
+            EventSystem.current.SetSelectedGameObject(GameObject.Find("quickButton"));
+        }
+
+        //exitDialogが有効なとき、左右入力があったとき
+        if (EventSystem.current.currentSelectedGameObject == null && (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow)) 
+            && GameObject.Find("exitDialog").GetComponent<Image>().raycastTarget) {
+
+            EventSystem.current.SetSelectedGameObject(GameObject.Find("negativeButton"));
+
+        }
+        
+        //選ばれてるGameObjectがなくなったとき、アニメーションを初期状態に戻す
+        if (EventSystem.current.currentSelectedGameObject == null && flag)
+        {
+            GameObject.Find("Canvas").GetComponent<Animator>().SetTrigger("idle");
+            flag = false;
+        }
+        else if(EventSystem.current.currentSelectedGameObject != null)
+        {
+            flag = true;
+        }
+
+    }
+
+}
