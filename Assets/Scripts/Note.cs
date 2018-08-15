09d3c040ef71871d9327191b15a752d1098fa044
Mitsuki Kurashige <az.kinosaki@gmail.com>
2018-08-15 23:51:23 +0900

    ノーツオブジェクト作成

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -1,7 +1,16 @@
-﻿public class Note 
-{
-    Note(uint value)
-    {
-        
-    }
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
 }
--- Assets/Scripts/gameNotesGenerater.cs
+++ Assets/Scripts/gameNotesGenerater.cs
@@ -1,14 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class gameNotesGenerater : MonoBehaviour {
 
-    private uint bpm, max, min, num, repeat;
-	private uint[] Notes;
+    private ushort bpm, num, repeat;
+	private byte max, min;
+	private byte[] Notes;
 
 	// Use this for initialization
-	void Start ()
+	public void Start ()
 	{
 
 		bpm = 120;
@@ -17,27 +20,34 @@ public class gameNotesGenerater : MonoBehaviour {
 		num = 20;
 		repeat = 1;
 		
-		Notes = new uint[num];
+		Notes = new byte[num];
 
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	public void Update () {
 
-
+		
 		
 	}
 
 	void Generate()
 	{
+
+		Notes = new byte[num];
+		
 		for (var i = 0; i < Notes.Length; i++)
 		{
 
-			var each = (short)Random.Range(min,max+1);
-
+			var each = (byte)Random.Range(min,max+1);
+			byte[] tmp = {1, 2, 4, 8, 16, 32, 64};
+			
 			for (var j = 0; j < each; j++)
 			{
-				uint[] tmp = {1, 2, 4, 8, 16, 32, 64};
+				var task = (byte) Random.Range(0, 7-j);
+				Notes[i] |= tmp[task];
+				tmp[task] = tmp[tmp.Length-(j+1)];
+
 			}
 
 		}
