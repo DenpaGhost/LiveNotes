7ef1d68cc52535ccf38c43def04b44861d9133c0
DenpaGhost <az.kinosaki@gmail.com>
2018-10-29 08:45:54 +0900

    [add]Piano ModuleとModuleBase

---
+++ Assets/Scripts/module/ModuleData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ModuleData
+{
+    private readonly string clipName;
+
+    public string ClipName => clipName;
+
+    public string ClipPath => path;
+
+    private readonly string path;
+
+    public ModuleData(string clipName, string path)
+    {
+        this.clipName = clipName;
+        this.path = path;
+    }
+}
